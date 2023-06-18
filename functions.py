8f08abb58229c9d624da723c8255fe5ef29acc9b
alanscheibler <amscheibler@gmail.com>
2023-06-18 10:55:18 -0300

    organização do código nos arquivos main e functions

--- functions.py
+++ functions.py
@@ -1,10 +1,116 @@
 import pygame as pg
 from pygame.locals import *
-
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import simpledialog, messagebox
+import os
+import random
 
 pg.init()
+pg.font.init()
 
-def textSetings():
-    font = pg.font.Font(None, 20)
+font = pg.font.Font(None, 20)
+distanceTxt = ""
+dotStarGP = pg.sprite.Group()
+distanceLineGP = pg.sprite.Group()
+distanceTextGP = pg.sprite.Group()
+running = True
+marked = []
+totalDist = 0
+starsFolder = "stars"
+starsPath = os.path.join(os.getcwd(), starsFolder)
+starsFile = os.listdir(starsPath)
+
+
+white = (250, 250, 250)
+black = (0, 0, 0)
+resolution = (800, 600)
+screen = pg.display.set_mode(resolution, pg.RESIZABLE)
+
+pg.display.set_caption("Space Marker")
+background = pg.image.load("bg1.png")
+iconExec = pg.image.load("shipSDOL2.png")
+pg.display.set_icon(iconExec)
+pg.mixer.music.load("Space_Machine_Power.mp3")
+pg.mixer.music.play(-1)
+
+class DotStar(pg.sprite.Sprite):
+    def __init__(self, loc, name):
+        super().__init__()  
+        self.image = pg.image.load(os.path.join(starsPath, random.choice(starsFile)))
+        self.rect = self.image.get_rect()
+        self.rect.center = loc
+        self.name = name
+
+    def printNameStar(self, screen):
+        text = font.render(self.name, True, white)
+        textBox = text.get_rect()
+        textBox.centerx = self.rect.centerx  
+        textBox.bottom = self.rect.top - 5  
+        screen.blit(text, textBox)
+
+def starsDataHistoric():
+    try:
+        dataHist = open ("dataHist.txt", "r")
+    except:
+        dataHist = open("dataHist.txt", "w")
+        dataHist.close()
+        dataHist = open("dataHist.txt", "r")
+    data = dataHist.readlines()
+    dataHist.close()
+    return data
+
+def createDictionary():
+    dictionaryName = simpledialog.askstring("Salvar conjunto de estrelas", "Digite o nome do conjunto de estrelas:" )
+    starData = {}
+    for index, star in enumerate(dotStarGP):
+        starData[index] = {
+            "loc": star.rect.center,
+            "name": star.name
+        }
+    data = starsDataHistoric()
+    data.append(dictionaryName + "\n")
+    data.append(str(starData) + "\n")
+    dataHist = open("dataHist.txt", "w")
+    dataHist.writelines(data)
+    dataHist.close()
+
+def loadDictionary(dataHist, dictionaryName):
+    try:
+        with open(dataHist, "r") as file:
+            lines = file.readlines()
+            data = {}
+            loading = False
+            for line in lines:
+                if loading:
+                    dictionary = eval(line.strip())
+                    data[dictionaryName].update(dictionary)
+                    loading = False
+                elif line.strip() == dictionaryName:
+                    loading = True
+                    data[dictionaryName] = {}
+            return data.get(dictionaryName, {})
+    except IOError:
+        messagebox.showinfo("Erro ao carregar conjunto")
+
+def summaryTxt():
+    summary = "F10 - Salvar"
+    summary1 = "F11 - Carregar Save"
+    summary2 = "F12 - Limpar tela"
+    summary3 = "LCTRL + Z - Apaga o último ponto"
+    sumText = font.render(summary, True, white)
+    sumText1 = font.render(summary1, True, white)
+    sumText2 = font.render(summary2, True, white)
+    sumText3 = font.render(summary3, True, white)
+    sumRect = sumText.get_rect()
+    sumRect1 = sumText1.get_rect()
+    sumRect2 = sumText2.get_rect()
+    sumRect3 = sumText3.get_rect()
+    sumRectLoc = (10, 10)
+    sumRectLoc1 = (10, 25)
+    sumRectLoc2 = (10, 40)
+    sumRectLoc3 = (10, 55)
+    screen.blit(sumText, sumRectLoc)
+    screen.blit(sumText1, sumRectLoc1)
+    screen.blit(sumText2, sumRectLoc2)
+    screen.blit(sumText3, sumRectLoc3)
+
--- main.py
+++ main.py
@@ -1,100 +1,27 @@
 import pygame as pg
 from pygame.locals import *
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import simpledialog, messagebox
 import os
 import random
 
+from functions import *
+
 pg.init()
 pg.font.init()
-
-font = pg.font.Font(None, 20)
-distanceTxt = ""
-dotStarGP = pg.sprite.Group()
-distanceLineGP = pg.sprite.Group()
-distanceTextGP = pg.sprite.Group()
-running = True
-marked = []
-totalDist = 0
-starsFolder = "stars"
-starsPath = os.path.join(os.getcwd(), starsFolder)
-starsFile = os.listdir(starsPath)
-
 
 white = (250, 250, 250)
 black = (0, 0, 0)
 resolution = (800, 600)
 screen = pg.display.set_mode(resolution, pg.RESIZABLE)
-
-pg.display.set_caption("Space Marker")
 background = pg.image.load("bg1.png")
 iconExec = pg.image.load("shipSDOL2.png")
+marked = []
+
+pg.display.set_caption("Space Marker")
 pg.display.set_icon(iconExec)
 pg.mixer.music.load("Space_Machine_Power.mp3")
 pg.mixer.music.play(-1)
-
-class DotStar(pg.sprite.Sprite):
-    def __init__(self, loc, name):
-        super().__init__()  
-        self.image = pg.image.load(os.path.join(starsPath, random.choice(starsFile)))
-        self.rect = self.image.get_rect()
-        self.rect.center = loc
-        self.name = name
-
-    def printNameStar(self, screen):
-        text = font.render(self.name, True, white)
-        textBox = text.get_rect()
-        textBox.centerx = self.rect.centerx  
-        textBox.bottom = self.rect.top - 5  
-        screen.blit(text, textBox)
-
-def starsDataHistoric():
-    try:
-        dataHist = open ("dataHist.txt", "r")
-    except:
-        dataHist = open("dataHist.txt", "w")
-        dataHist.close()
-        dataHist = open("dataHist.txt", "r")
-    data = dataHist.readlines()
-    dataHist.close()
-    return data
-
-def createDictionary():
-    dictionaryName = simpledialog.askstring("Salvar conjunto de estrelas", "Digite o nome do conjunto de estrelas:" )
-    starData = {}
-    for index, star in enumerate(dotStarGP):
-        starData[index] = {
-            "loc": star.rect.center,
-            "name": star.name
-        }
-    data = starsDataHistoric()
-    data.append(dictionaryName + "\n")
-    data.append(str(starData) + "\n")
-    dataHist = open("dataHist.txt", "w")
-    dataHist.writelines(data)
-    dataHist.close()
-
-def loadDictionary(dataHist, dictionaryName):
-    try:
-        with open(dataHist, "r") as file:
-            lines = file.readlines()
-            data = {}
-            loading = False
-            for line in lines:
-                if loading:
-                    dictionary = eval(line.strip())
-                    data[dictionaryName].update(dictionary)
-                    loading = False
-                elif line.strip() == dictionaryName:
-                    loading = True
-                    data[dictionaryName] = {}
-            return data.get(dictionaryName, {})
-    except IOError:
-        loadError = "Erro ao carregar o arquivo"
-        loadErrorTXT = font.render(loadError, True, white)
-        LoadErrorRect = sumText.get_rect()
-        LoadErrorRectLoc = (400, 300)
-        screen.blit(loadErrorTXT, LoadErrorRectLoc)
 
 class DistanceLine(pg.sprite.Sprite):
     def __init__(self, firstPoint, secondPoint):
@@ -131,12 +58,12 @@ def calculateDistace():
                 distanceLen = distance.length()
                 distanceTxt = DistanceTxt((firstPoint + secondPoint) / 2, "Distancia: {:.2f}".format(distanceLen))
                 distanceTextGP.add(distanceTxt)
-
+                
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT or event.type == pg.KEYUP and event.key == pg.K_ESCAPE:
             saveQuit = simpledialog.askstring("Salvar", "Deseja salvar antes de sair? r: s ou n")
-            if saveQuit == "s" or "S" or "sim" or "Sim":
+            if saveQuit == "s" or saveQuit == "S":
                 starsDataHistoric()
                 createDictionary()
                 running = False
@@ -183,16 +110,19 @@ while running:
                   
         elif event.type == MOUSEBUTTONUP:
             if event.button == 1:
-                loc = event.pos
-                nameStar = simpledialog.askstring("Nome", "Digite o nome da estrela: ")
                 try:
-                    if nameStar.strip() == "":
+                    loc = event.pos
+                    nameStar = simpledialog.askstring("Nome", "Digite o nome da estrela: ")
+                    try:
+                        if nameStar.strip() == "":
+                            nameStar = "Desconhecido"
+                    except tk.TclError:
                         nameStar = "Desconhecido"
-                except tk.TclError:
-                    nameStar = "Desconhecido"
-                star = DotStar(loc, nameStar)
-                dotStarGP.add(star)  
-                marked.append(loc)
+                    star = DotStar(loc, nameStar)
+                    dotStarGP.add(star)  
+                    marked.append(loc)
+                except:
+                    nameStar.destroy()
 
     screen.fill(black)
     screen.blit(background,(0,0))
@@ -200,18 +130,11 @@ while running:
     distanceLineGP.draw(screen)
     distanceTextGP.draw(screen)
 
-    calculateDistace()
-    
-
     for star in dotStarGP:
         star.printNameStar(screen)
 
-    summary = "F10 - Salvar // F11 - Carregar Save // F12 - Limpar tela // LCTRL + Z - Apaga o último ponto"
-    sumText = font.render(summary, True, white)
-    sumRect = sumText.get_rect()
-    sumRectLoc = (10, 10)
-    screen.blit(sumText, sumRectLoc)
-
+    calculateDistace()
+    summaryTxt()
     pg.display.update()
 
 
